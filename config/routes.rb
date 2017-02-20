c65c40036ebbac832ff87c462bbfb22d3ba7b4c2
keiji.ue <keiji.ue@gmail.com>
2017-02-20 08:45:40 +0000

    add game

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :games
   resources :books
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
